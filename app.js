0e6abcbfaae53e1bf72c1c4785a9175ec456ca66
Tomeu Cabot <tcabot@xmltravelgate.com>
2016-10-21 10:19:31 +0200

    add env config

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var RtmClient = require('@slack/client').RtmClient;
 var jsdom = require("node-jsdom");
 
 // The memory data store is a collection of useful functions we can include in our RtmClient
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
-
+var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+config = require('./config');
 
-var token = 'private';
-
-var rtm = new RtmClient(token, {
+var rtm = new RtmClient(config.env.token, {
   // Sets the level of logging we require
   logLevel: 'error',
   // Initialise a data store for our client, this will load additional helper functions for the storing and retrieval of data
@@ -18,7 +17,7 @@ var rtm = new RtmClient(token, {
 rtm.start();
 
 // Wait for the client to connect
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
   // Get the user's name
   var user = rtm.dataStore.getUserById(rtm.activeUserId);
 
@@ -30,7 +29,6 @@ rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
 });
 
 
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   // Listens to all `message` events from the team
@@ -39,38 +37,43 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   let text = message.text;
   let user = rtm.dataStore.getUserById(message.user)
 
-  let condition = text.includes('bot-tomeu') && text.includes('dinar');
+  let condition = text.includes(config.env.bot_name) && text.includes('dinar');
 
   if (condition) {
     jsdom.env({
-      url: "private",
+      url: config.env.url,
       scripts: ["http://code.jquery.com/jquery.js"],
       done: function (errors, window) {
-          var $ = window.$;
+        var $ = window.$;
 
-          let items = $(".w-pricing-item-features")
-          let rows = []
-          items.each(index => {
-            rows.push(items[index].children._toArray().map(x => {return x.innerHTML;}))
-          })
-          let headers = $(".w-pricing-item-title")
-          headers.each((index) => rows[index].unshift(headers[index].innerHTML))
+        let items = $(".w-pricing-item-features")
+        let rows = []
+        items.each(index => {
+          rows.push(items[index].children._toArray().map(x => { return x.innerHTML; }))
+        })
+        let headers = $(".w-pricing-item-title")
+        headers.each((index) => rows[index].unshift(headers[index].innerHTML))
 
-          let date = new Date();
-          if (text.includes('dema')) date.setDate(date.getDate() + 1)
-          let today = rows.find(x => x[0].includes(date.getDate()))
-          console.log(`date asked: ${date.getDate()}`)
-          let message;
-          if (today != null){
-            message = '```' + today.reduce((prev,curr) => prev += curr + '\n') + '```'
-          }else {
-            message = `Avui, ${date.toISOString().slice(0, 10)}, no hi ha dinar`
-          }
-          rtm.sendMessage(message, channel, function messageSent() {});
+        let date = new Date();
+        if (text.includes('dema')) date.setDate(date.getDate() + 1)
+        let today = rows.find(x => x[0].includes(date.getDate()))
+        console.log(`date asked: ${date.getDate()}`)
+        let message;
+        if (today != null) {
+          message = '```' + today.reduce((prev, curr) => prev += curr + '\n') + '```'
+        } else {
+          message = `Dema, ${date.toISOString().slice(0, 10)}, no hi ha dinar`
         }
-      });
+        rtm.sendMessage(message, channel, function messageSent() { });
+      }
+    });
 
 
     // setTimeout(() => rtm.sendMessage(`que paso wey!`, channel, null), 2000)
   }
 });
+
+// this.slack.on("close", (code, reason) => {
+//   console.log('connection closed' + code + ' - ' + reason)
+//   this.slack.reconnect()
+// })
