393873a6e13b2130d2372b06169bbbf922224445
Tomeu Cabot <tcabot@xmltravelgate.com>
2017-01-27 19:04:56 +0100

    update & add more fun fun functions

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const jsdom = require("node-jsdom");
 // 4 ajax
 const fetch = require('node-fetch');
 
-
 // The memory data store is a collection of useful functions we can include in our RtmClient
 const MemoryDataStore = require('@slack/client').MemoryDataStore;
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-config = require('./config');
+const config = require('./config');
 
 const rtm = new RtmClient(config.env.token, {
   // Sets the level of logging we require
@@ -24,31 +23,48 @@ rtm.start();
 // Wait for the client to connect
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
   // Get the user's name
-  let user = rtm.dataStore.getUserById(rtm.activeUserId);
-
+  const user = rtm.dataStore.getUserById(rtm.activeUserId);
   // Get the team's name
-  let team = rtm.dataStore.getTeamById(rtm.activeTeamId);
-
+  const team = rtm.dataStore.getTeamById(rtm.activeTeamId);
   // Log the slack team name and the bot's name
   console.log('Connected to ' + team.name + ' as ' + user.name);
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-  let channel = message.channel;
 
-  let text = message.text;
-  if (!text) return;
+  if (!message.text || !message.text.includes(config.env.bot_name)) return;
 
-  let user = rtm.dataStore.getUserById(message.user)
+  const channel = message.channel;
+  const text = message.text;
+  const user = rtm.dataStore.getUserById(message.user)
 
-  if (!text.includes(config.env.bot_name)) return; // we're not called 
-
-  if (text.includes('dinar')) {
-    getLaunch(text, channel);
-  }
-
-  if (text.includes('boobs') || text.includes('tetas')) {
-    getBoobs(channel);
+  switch (true) {
+    case text.includes('dinar'):
+    case text.includes('launch'):
+    case text.includes('menu'):
+      console.log('Asked for launch ->', user.profile.first_name);
+      getLaunch(text, channel)
+      break;
+    case text.includes('boob'):
+    case text.includes('teta'):
+      console.log('Asked for boobs ->', user.profile.first_name)
+      getBoobs(channel)
+      break;
+    case text.includes('top20'):
+      console.log('Asked for top20 ->', user.profile.first_name)
+      top20(channel);
+    break;
+    case text.includes('butt'):
+    case text.includes('culo'):
+      console.log('Asked for butts ->', user.profile.first_name)
+      getButts(channel)
+      break;
+    case text.includes('fuck you'):
+      fuckYou(channel);
+      break;
+    default:
+      console.log(`K putes dises ${user.profile.first_name}!?`)
+      rtm.sendMessage(`K putes dises ${user.profile.first_name}!?\n\`\`\`Pots demanar: menu, tetas, top20 o culo. \`\`\``, channel, () => {});
   }
 
 
@@ -59,55 +75,64 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
 function getLaunch(text, channel) {
   jsdom.env({
     url: config.env.url,
-    scripts: ["http://code.jquery.com/jquery.js"],
+    scripts: [],
     done: function (errors, window) {
-      var $ = window.$;
-
-      let items = $(".w-pricing-item-features")
-      let rows = []
-      items.each(index => {
-        rows.push(items[index].children._toArray().map(x => { return x.innerHTML; }))
+      const boxes = window.document.querySelectorAll('.w-pricing-item-h')._toArray()
+      const menus = boxes.map(box => {
+        const date = box.querySelector('.w-pricing-item-title').textContent.trim();
+        const dishes = box.querySelectorAll('.w-pricing-item-feature')._toArray()
+          .map(x => x.textContent)
+        return {
+          date: date,
+          dishes: dishes
+        }
       })
-      let headers = $(".w-pricing-item-title")
-      headers.each((index) => rows[index].unshift(headers[index].innerHTML))
+      const menu = menus.find(menu => menu.date.includes(new Date().getDay()))
 
-      let date = new Date();
-      let dema = text.includes('dema')
-      if (dema) date.setDate(date.getDate() + 1)
-      let today = rows.find(x => x[0].includes(date.getDate()))
-      let message;
-      if (today != null) {
-        console.log(today);
-        let d = today.shift().trim().replace('\n', '');
-        message = '```'
-        message += `+---------------------------------------------+\n`
-        message += `${d} | Tiempo restante: (${diffBetweenDates()})\n`
-        message += `+---------------------------------------------+\n`
-        message += `${today.reduce((prev, curr, i, array) => prev += '\n' + curr)}`
-        message += '```'
-        message += `\n:knife_fork_plate: ${config.env.url}`
-
-      } else {
-        message = `${dema? 'Dema' : 'Avui'}, ${date.toISOString().slice(0, 10)}, no hi ha dinar`
+      if (!menu) {
+        rtm.sendMessage('No hi ha dinar avui... :fu:', channel, () => { })
+        return;
       }
+
+      const message = `
+        \`\`\`
+        ${menu.dishes.reduce((pre, curr) => { return pre += curr + '\n' }, '')}
+        \`\`\`
+      `
+
       rtm.sendMessage(message, channel, function messageSent() { });
     }
   });
 }
 
 function getBoobs(channel) {
-
-  let max = 2000;
-  let min = 1
-  let n = Math.random() * (max - min) + min;
-  n = Math.floor(n);
-  fetch(`http://api.oboobs.ru/boobs/${n}`)
-    .then(data => {
-      return data.json();
-    })
-    .then(data => rtm.sendMessage(`http://media.oboobs.ru/${data[0].preview}`, channel, () => { }))
+  // TOP 20
+  // http://api.oboobs.ru/boobs/0/20/-interest/
+  const id = Math.floor((Math.random() * (3500 - 1) + 1));
+  fetch(`http://api.oboobs.ru/boobs/${id}`)
+    .then(data => data.json())
+    .then(data => rtm.sendMessage(`Model: ${data[0].model || '#404 - NOT FOUND'} - Rank: ${data[0].rank}\nhttp://media.oboobs.ru/${data[0].preview}`, channel, () => { }))
     .catch(err => console.log(err))
+}
+/**
+ * Get random picture of top 20 boobs
+ */
+function top20(channel) {
+  // TOP 20
+  // http://api.oboobs.ru/boobs/0/20/-interest/
+  const id = Math.floor((Math.random() * (20 - 1) + 1));
+  fetch(`http://api.oboobs.ru/boobs/0/20/-interest/`)
+    .then(data => data.json())
+    .then(data => rtm.sendMessage(`Model: ${data[id - 1].model || '#404 - NOT FOUND'} - Rank: ${data[id - 1].rank}\nhttp://media.oboobs.ru/${data[id-1].preview}`, channel, () => { }))
+    .catch(err => console.log(err))
+}
 
+function getButts(channel) {
+  const id = Math.floor((Math.random() * (3500 - 1) + 1));
+  fetch(`http://api.obutts.ru/butts/${id}`)
+    .then(data => data.json())
+    .then(data => rtm.sendMessage(`Model: ${data[0].model || '#404 - NOT FOUND'} - Rank: ${data[0].rank}\nhttp://media.obutts.ru/${data[0].preview}`, channel, () => { }))
+    .catch(err => console.log(err))
 }
 
 function diffBetweenDates() {
@@ -136,4 +161,15 @@ function diffBetweenDates() {
 
     return addZeros(hrs) + ':' + addZeros(mins) + ':' + addZeros(secs)
   }
+}
+
+function fuckYou(channel) {
+  fetch(`https://api.giphy.com/v1/gifs/search?q=fuck+you&api_key=dc6zaTOxFJmzC`)
+    .then(data => data.json())
+    .then(res => {
+      const id = Math.floor(Math.random() * (res.data.length - 1) + 1)
+      console.log(res.data[id].embed_url)
+      rtm.sendMessage(`FUCK YOU!\n${res.data[id].url}`, channel, () => { })
+    })
+    .catch(err => console.log(err))
 }
